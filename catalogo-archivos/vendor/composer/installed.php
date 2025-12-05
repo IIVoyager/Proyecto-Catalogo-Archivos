8c0cf4d1490f07061aa68d3f4d9ae5f8060c0dfb
IIVoyager <ac5151832@gmail.com>
2025-12-05 00:02:03 -0600

    Implementación de composer

---
+++ catalogo-archivos/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'catalogo-archivos/sistema',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fd94a557abced5909503aba4561af728ab17e8ae',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'catalogo-archivos/sistema' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fd94a557abced5909503aba4561af728ab17e8ae',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
